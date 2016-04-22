e448d38606b38624c2c8f5612b2aff5ecdef0ad0
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2016-04-22 14:08:06 +0200

    all include and declaration for pkg building

---
+++ pkg/inst/include/countMatrixFactor.h
@@ -0,0 +1,21 @@
+#include <RcppEigen.h>
+#include <math.h>
+#include <iostream>
+#include <string>
+//#include <typeinfo>
+//#include <functional>
+#include <boost/math/special_functions/digamma.hpp>
+
+using namespace Rcpp;
+using namespace std;
+
+using boost::math::digamma;
+
+using Eigen::Map;                       // 'maps' rather than copies
+using Eigen::MatrixXd;                  // variable size matrix, double precision
+using Eigen::MatrixXi;                  // variable size matrix, integer
+using Eigen::VectorXd;                  // variable size vector, double precision
+using Eigen::VectorXi;                  // variable size vector, double precision
+//using Eigen::JacobiSVD;                 // svd decomposition
+//using Eigen::ComputeThinU;              // svd decomposition: compute U
+//using Eigen::ComputeThinV;              // svd decomposition: compute V
