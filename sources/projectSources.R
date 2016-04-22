1ba57e6e0f3b48767dfa1419394f19853a44a242
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2016-04-22 11:28:47 +0200

    script to source all functions (in R and Cpp)

---
+++ sources/projectSources.R
@@ -0,0 +1,51 @@
+# project hierarchical clustering
+# sourcing all files (R and Cpp)
+
+source("/home/durif/source_code/countMatrixFactor/set_working_dir.R")
+
+project.sources = function(subpath="pkg") {
+
+    ## library
+
+
+    ########################################################################
+    ##### Cpp
+    ########################################################################
+
+    library(Rcpp)
+
+    ## path to Cpp src
+    src.path = paste0(WORKINGDIR, "/", subpath, "/src/")
+
+    ## list of R files
+    file.list = system(paste0("cd ", src.path, " && git ls-files | grep \"\\\\.cpp\""), intern=TRUE)
+
+    file.list = file.list[! file.list %in% c("aaa")]
+
+    print(file.list)
+
+    ## sourcing all R files
+    path.list = paste0(src.path, file.list)
+    sapply(path.list, sourceCpp)
+
+
+
+    ########################################################################
+    ##### R
+    ########################################################################
+
+
+    ## path to R src
+    src.path = paste0(WORKINGDIR, "/", subpath, "/R/")
+
+    ## list of R files
+    file.list = system(paste0("cd ", src.path, " && git ls-files | grep \"\\\\.R\""), intern=TRUE)
+
+    ## sourcing all R files
+    path.list = paste0(src.path, file.list)
+    sapply(path.list, source)
+
+
+}
+
+project.sources()
