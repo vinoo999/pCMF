808a5c20d7fde41aa9985b28444fba995f693b5e
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2016-04-22 15:24:22 +0200

    definition of member functions

--- pkg/src/gamPoisFactor.h
+++ pkg/src/gamPoisFactor.h
@@ -30,6 +30,11 @@
 #include <Rcpp.h>
 #include <RcppEigen.h>
 
+// [[Rcpp::depends(RcppEigen)]]
+using Eigen::MatrixXd;                  // variable size matrix, double precision
+using Eigen::MatrixXi;                  // variable size matrix, integer
+using Eigen::VectorXd;                  // variable size vector, double precision
+
 /*!
  * \namespace countMatrixFactor
  *
@@ -61,7 +66,8 @@ namespace countMatrixFactor {
 
         // parameters
         int m_iterMax;        /*!< maximum number of iterations */
-        int m_order;          /*!< derivative order on normalized gap to assess convergence (0 is the current value, 1 the first order empirical derivative, 2 the second order empirical derivative) */
+        int m_order;          /*!< derivative order on normalized gap to assess convergence
+                                (0 is the current value, 1 the first order empirical derivative, 2 the second order empirical derivative) */
         int m_stabRange;      /*!< range of stability (number of iterations where parameter values are stable to confirm convergence) */
         double m_epsilon;     /*!< precision for comparison when assessing convergence */
         bool m_verbose;       /*!< boolean indicating verbosity in the output */
@@ -86,13 +92,13 @@ namespace countMatrixFactor {
         MatrixXd m_EU;            /*!< n x K, Expectation of U */
         MatrixXd m_ElogU;         /*!< n x K, Expectation of log U */
         MatrixXd m_EV;            /*!< p x K, Expectation of V */
-        MAtrixXd m_ElogV;         /*!< p x K, Expectation of log V */
+        MatrixXd m_ElogV;         /*!< p x K, Expectation of log V */
 
         MatrixXd m_EZ_i;          /*!< p x k, \sum_i X_{ij} xi_{ijk} = \sum_i E[Z_{ijk}] */
         MatrixXd m_EZ_j;          /*!< n x k, \sum_j X_{ij} xi_{ijk} = \sum_j E[Z_{ijk}] */
 
         // prior parameter
-        MatriXd m_alpha1;         /*!< n x K, values of first parameter of Gamma prior on U */
+        MatrixXd m_alpha1;         /*!< n x K, values of first parameter of Gamma prior on U */
         MatrixXd m_alpha2;        /*!< n x K, values of second parameter of prior Gamma prior on U */
         MatrixXd m_beta1;         /*!< p x K, values of first parameter of prior Gamma prior on V */
         MatrixXd m_beta2;         /*!< p x K, values of second parameter of prior Gamma prior on V */
@@ -116,9 +122,93 @@ namespace countMatrixFactor {
          *
          * Constructor of the class gamPoisFactor
          *
-         * \param
+         * \param n number of observations (rows)
+         * \param p number of variables (columns)
+         * \param K dimension of the latent subspace
+         * \param iterMax maximum number of iterations
+         * \param order derivative order on normalized gap to assess convergence
+         * (0 is the current value, 1 the first order empirical derivative, 2 the second order empirical derivative)
+         * \param stabRange range of stability (number of iterations where parameter values are stable to confirm convergence)
+         * \param epsilon precision for comparison when assessing convergence
+         * \param verbose boolean indicating verbosity in the output
+         * \param X n x p, count data matrix (const reference)
+         * \param phi1 n x K, initial values of first parameter of Gamma distribution on U (const reference)
+         * \param phi2 n x K, initial values of second parameter of Gamma distribution on U (const reference)
+         * \param theta1 n x K, initial values of first parameter of Gamma distribution on V (const reference)
+         * \param theta2 n x K, initial values of second parameter of Gamma distribution on V (const reference)
+         * \param alpha1 n x K, initial values of first parameter of Gamma prior on U (const reference)
+         * \param alpha2 n x K, initial values of second parameter of Gamma prior on U (const reference)
+         * \param beta1 n x K, initial values of first parameter of Gamma prior on V (const reference)
+         * \param beta2 n x K, initial values of second parameter of Gamma prior on V (const reference)
          */
-        gamPoisFactor(int n, in)
+        gamPoisFactor(int n, int p, int K, int iterMax, int order,
+                      int stabRange, double epsilon, bool verbose,
+                      const MatrixXi &X,
+                      const MatrixXd &phi1, const MatrixXd &phi2,
+                      const MatrixXd &theta1, const MatrixXd &theta2,
+                      const MatrixXd &alpha1, const MatrixXd &alpha2,
+                      const MatrixXd &beta1, const MatrixXd &beta2);
+
+        /*!
+         * \brief Destructor
+         *
+         * Destructor of the class gamPoisFactor
+         */
+        ~gamPoisFactor();
+
+    protected:
+        // member functions: documented in src
+
+        // initialization
+        void Init();
+
+        //-------------------//
+        // sufficient stats  //
+        //-------------------//
+
+        // Expectation gamma
+        void Egamma(const MatrixXd &alpha, const MatrixXd &beta, MatrixXd &res);
+
+        // Expectation log gamma
+        void Elgamma(const MatrixXd &alpha, const MatrixXd &beta, MatrixXd &res);
+
+        //-------------------//
+        //   convergence     //
+        //-------------------//
+
+        // parameter norm
+        double parameterNorm(const MatrixXd &phi1, const MatrixXd &phi2,
+                             const MatrixXd &theta1, const MatrixXd &theta2);
+
+        // difference norm (on parameters)
+        double differenceNorm(const MatrixXd &phi1old, const MatrixXd &phi2old,
+                              const MatrixXd &theta1old, const MatrixXd &theta2old,
+                              const MatrixXd &phi1new, const MatrixXd &phi2new,
+                              const MatrixXd &theta1new, const MatrixXd &theta2new);
+
+        // convergence condition
+        double convCondition(int order, const VectorXd &normGap, int iter, int drift);
+
+        //-------------------//
+        // parameter updates //
+        //-------------------//
+
+        // Poisson intensity
+        void poisRate(int n, int p, int K,
+                      MatrixXd &EZ_i, MatrixXd &EZ_j,
+                      const MatrixXd &ElogU, const MatrixXd &ElogV);
+
+        // local parameters: phi (factor U)
+        void localParam(int n, int p, int K,
+                        MatrixXd &phi1cur, MatrixXd &phi2cur,
+                        const MatrixXd &EZ_j, const MatrixXi &X, const MatrixXd &EV,
+                        const MatrixXd &alpha1, const MatrixXd &alpha2);
+
+        // global parameters: theta (factor V)
+        void globalParam(int n, int p, int K,
+                         MatrixXd &theta1cur, MatrixXd &theta2cur,
+                         const MatrixXd &EZ_j, const MatrixXi &X, const MatrixXd &EU,
+                         const MatrixXd &beta1, const MatrixXd &beta2);
 
 
     };
