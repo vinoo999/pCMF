af233c0a625682e42c2f7c6df3827f8ef11b0491
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2016-06-02 16:25:33 +0200

    function to plot matrix as heatmap

---
+++ pkg/R/matrixHeatmap.R
@@ -0,0 +1,37 @@
+### Copyright 2016-06 Ghislain DURIF
+###
+### This file is part of the `countMatrixFactor' library for R and related languages.
+### It is made available under the terms of the GNU General Public
+### License, version 2, or at your option, any later version,
+### incorporated herein by reference.
+###
+### This program is distributed in the hope that it will be
+### useful, but WITHOUT ANY WARRANTY; without even the implied
+### warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+### PURPOSE.  See the GNU General Public License for more
+### details.
+###
+### You should have received a copy of the GNU General Public
+### License along with this program; if not, write to the Free
+### Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+### MA 02111-1307, USA
+
+#' @title matrixHeatmap
+#'
+#' @description
+#' plot the entries of a matrix as a heatmap
+#'
+#' @details
+#'
+#' @author
+#' Ghislain Durif, \email{ghislain.durif@univ-lyon1.fr}
+#'
+#' @importFrom fields image.plot
+#'
+#' @param mat the matrix to plot
+#'
+
+#' @export
+matrixHeatmap <- function(mat, ...) {
+    image.plot(t(apply(mat, 2, rev)), xaxt="n", yaxt="n", asp = 1, ...)
+}
