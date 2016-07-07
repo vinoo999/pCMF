8ea178167b3c46671065ede656a85ae62427b189
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2016-07-07 16:36:04 +0200

    testing logit function

---
+++ scripts/tests/various/test_logit.R
@@ -0,0 +1,17 @@
+curve((1-x)*log(x/(1-x)), from=0, to=1)
+curve(expit(log(x*0.001/(1-x))), from=0.99, to=1)
+curve(exp(x)/(1+exp(x)), from=-10, to=10)
+
+
+logit = function(x) {
+    return(log(x/(1-x)))
+}
+
+expit = function(x) {
+    exp(x)/(1+exp(x))
+}
+
+p = 0.8
+logit(p)
+
+expit(0.9)
