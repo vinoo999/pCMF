4196c89174df66441e23feeeb4f0987dac4d26f0
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2016-08-04 17:41:15 +0200

    test on solving system with inverse of digamma

--- scripts/tests/various/test_inverse_digamma.R
+++ scripts/tests/various/test_inverse_digamma.R
@@ -13,15 +13,55 @@ psiInv = function(y, iter=5) {
         x[i+1] = x[i] - (digamma(x[i]) - y)/trigamma(x[i])
     }
 
-    return(x)
+    return(tail(x,1))
 }
 
 y = digamma(0.0000002)
 y
 psiInv(y)
 
+digammaInv = function(y) {
+    return(sapply(y, psiInv))
+}
+
 
 
 y = c(digamma(0.478), digamma(1), digamma(1.54895158), digamma(20))
 y
 sapply(y, psiInv)
+
+curve(digamma(x), from=0, to=10)
+curve(digammaInv(x), from=0, to=10)
+
+
+#### test system resolution with inverse of digamma
+K=4
+
+a1 = numeric(100)
+a2 = numeric(100)
+
+a1[1] = 5
+a2[1] = 4
+
+for(i in 1:99) {
+    a1[i+1] = psiInv(log(a2[i]) + 3)
+    a2[i+1] = a1[i+1] / 10
+}
+
+at1 = numeric(100)
+at2 = numeric(100)
+
+at1[1] = a1[1] / sqrt(K)
+at2[1] = a2[1] / sqrt(K)
+
+for(i in 1:99) {
+    at1[i+1] = psiInv(log(sqrt(K)*at2[i]) + 3)
+    at2[i+1] = at1[i+1] / (sqrt(K) * 10)
+}
+
+a1[100]
+at1[100] * sqrt(K)
+
+a2[100]
+at2[100] * sqrt(K)
+
