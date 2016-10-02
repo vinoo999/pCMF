b8d451d54da15dfc1ecc5202c997c5ebfce2bec9
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2016-10-03 00:11:39 +0200

    running tests

--- scripts/tests/test_gamPoisFactorSparse.R
+++ scripts/tests/test_gamPoisFactorSparse.R
@@ -26,7 +26,7 @@ blockAlpha1 <- blockMatrix(nrow=n, ncol=K, nRowBlock=nblock, nColBlock=nblock, s
 alpha1 <- blockAlpha1$mat
 alpha2 <- matrix(1, nrow=n, ncol=K)
 
-signalBlock <- matrix(0, nblock, nblock) + diag(8, nblock)
+signalBlock <- matrix(4, nblock, nblock) + diag(8, nblock)
 colOrder <- sample.int(n=nblock, size=nblock, replace=FALSE)
 signalBlockB <- signalBlock[,colOrder]
 blockBeta1 <- blockMatrix(nrow=p, ncol=K, nRowBlock=nblock, nColBlock=nblock, signalBlock=signalBlockB)
@@ -51,25 +51,25 @@ p=p+10
 # matrixHeatmap(data1$V, xlab="k = 1...K", ylab="i = 1...n")
 
 ####### TESTING ALGO
-ncomp=5
+ncomp=4
 
-alpha01 = matrix(1.2, nrow=n, ncol=ncomp)
-alpha02 = matrix(1.2, nrow=n, ncol=ncomp)
+alpha01 = matrix(1, nrow=n, ncol=ncomp)
+alpha02 = matrix(1, nrow=n, ncol=ncomp)
 
-beta01 = matrix(1.2, nrow=p, ncol=ncomp)
-beta02 = matrix(1.2, nrow=p, ncol=ncomp)
+beta01 = matrix(1, nrow=p, ncol=ncomp)
+beta02 = matrix(1, nrow=p, ncol=ncomp)
 
-phi01 = matrix(1.2, nrow=n, ncol=ncomp)
-phi02 = matrix(1.2, nrow=n, ncol=ncomp)
+phi01 = matrix(1, nrow=n, ncol=ncomp)
+phi02 = matrix(1, nrow=n, ncol=ncomp)
 
-theta01 = matrix(1.2, nrow=p, ncol=ncomp)
-theta02 = matrix(1.2, nrow=p, ncol=ncomp)
+theta01 = matrix(1, nrow=p, ncol=ncomp)
+theta02 = matrix(1, nrow=p, ncol=ncomp)
 
 res1 = matrixFactor(X, ncomp, phi01, phi02, theta01, theta02, alpha01, alpha02, beta01, beta02,
-                    iterMax=100, epsilon=1e-4, algo="EM", verbose=TRUE, sparse=TRUE)
+                    iterMax=200, iterMin=100, epsilon=1e-4, algo="EM", verbose=TRUE, sparse=TRUE)
 
 res2 = matrixFactor(X, ncomp, phi01, phi02, theta01, theta02, alpha01, alpha02, beta01, beta02,
-                    iterMax=100, epsilon=1e-4, algo="EM", verbose=TRUE, sparse=FALSE)
+                    iterMax=200, iterMin=100, epsilon=1e-4, algo="EM", verbose=TRUE, sparse=FALSE)
 
 str(res1)
 
@@ -77,29 +77,46 @@ print(res1$criteria_k$kDeviance)
 myOrder = res1$order$orderDeviance
 print(myOrder)
 
-matrixHeatmap(res1$sparseParams$probSparse)
-
 res1$sparseParams$probSparse
 res1$sparseParams$probSparsePrior
 res1$sparseParams$sparseIndic
 
-# layout(matrix(1:2,ncol=2))
-#
-# matrixHeatmap(res1$V)
-# matrixHeatmap(res2$V)
-#
-# matrixHeatmap(res1$U)
-# matrixHeatmap(res2$U)
-#
-# matrixHeatmap(res1$params$theta1)
-# matrixHeatmap(res2$params$theta1)
-#
-# matrixHeatmap(res1$params$theta2)
-# matrixHeatmap(res2$params$theta2)
-#
-# matrixHeatmap(res1$params$beta1)
-# matrixHeatmap(res1$params$beta2)
-# matrixHeatmap(res1$params$theta1/res1$params$theta2)
+layout(matrix(1:2,ncol=2))
+plot(res1$logLikelihood$elbo[-(1:2)], xlab="iteration", ylab="elbo", col="blue", type="l")
+plot(res1$normGap[-1], xlab="iteration", ylab="normalized gap", col="blue", type="b", log="y")
+
+matrixHeatmap(res1$sparseParams$probSparse)
+matrixHeatmap(res1$sparseParams$sparseIndic)
+
+matrixHeatmap(res1$V)
+matrixHeatmap(res2$V)
+
+matrixHeatmap(res1$U)
+matrixHeatmap(res2$U)
+
+matrixHeatmap(res1$params$theta1)
+matrixHeatmap(res2$params$theta1)
+
+matrixHeatmap(res1$params$theta2)
+matrixHeatmap(res2$params$theta2)
+
+matrixHeatmap(res1$params$beta1)
+matrixHeatmap(res2$params$beta1)
+
+matrixHeatmap(res1$params$beta2)
+matrixHeatmap(res2$params$beta2)
+
+matrixHeatmap(res1$params$phi1)
+matrixHeatmap(res2$params$phi1)
+
+matrixHeatmap(res1$params$phi2)
+matrixHeatmap(res2$params$phi2)
+
+matrixHeatmap(res1$params$alpha1)
+matrixHeatmap(res2$params$alpha1)
+
+matrixHeatmap(res1$params$alpha2)
+matrixHeatmap(res2$params$alpha2)
 
 
 
@@ -114,7 +131,7 @@ res1$sparseParams$sparseIndic
 #
 # plot(res1$logLikelihood$margLogLike, xlab="iteration", ylab="complete log likelihood", col="blue", type="l")
 #
-# plot(res1$logLikelihood$elbo, xlab="iteration", ylab="elbo", col="blue", type="l")
+# plot(res1$logLikelihood$elbo[-(1:2)], xlab="iteration", ylab="elbo", col="blue", type="l")
 #
 # ## norm gap
 # plot(res1$normGap[-1], xlab="iteration", ylab="normalized gap", col="blue", type="b", log="y")
