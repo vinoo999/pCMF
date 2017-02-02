4d1cd3f9346fdd386bc0fb13338c5592fbc792dd
Ghislain DURIF <ghislain.durif@univ-lyon1.fr>
2017-02-02 23:24:30 +0100

    files from countMatrixFactor

--- draft/modelSelection.R
+++ draft/modelSelection.R
@@ -26,7 +26,7 @@ icl <- function(model, X) {
     V <- model$V
     lambda <- model$lambda
 
-    K0 <- ncol(U)
+    K <- ncol(U)
 
     # alpha1 <- model$params$alpha1
     # alpha2 <- model$params$alpha2
@@ -40,21 +40,11 @@ icl <- function(model, X) {
     beta1 <- model$params$theta1
     beta2 <- model$params$theta2
 
-    nu_K <- K0 * (n+p)
-
-    cst <- log(K0)
-    # cst <- log(K0)
-
-    pen1 <- (nu_K/2) * (cst)
-    pen2 <- K0 * log(n) + jeffreys(alpha1[1,],alpha2[1,]) + jeffreys(beta1[1,],beta2[1,]) - (nu_K/2) * (cst)
-
-
     ### compute criterion
-    res1 <- poisLoglike(X, lambda) + gammaLoglike(U, alpha1, alpha2) + gammaLoglike(V, beta1, beta2)
-    res2 <- res1 - pen1
-    res3 <- res1 + pen2
+    res1 <- poisLoglike(X, lambda) + gammaLoglike(U, alpha1, alpha2) + gammaLoglike(V, beta1, beta2) - ((K * (n+p))/2) * (log(n*p) + 2*log(2) + log(2*pi))
+    res2 <- poisLoglike(X, lambda) + gammaLoglike(U, alpha1, alpha2) + gammaLoglike(V, beta1, beta2)
 
-    return(c(res1, res2, res3))
+    return(c(res1, res2))
 }
 
 
@@ -67,17 +57,12 @@ bic <- function(model, X) {
     V <- model$V
     lambda <- model$lambda
 
-    K0 <- ncol(U)
-
-    nu_K <- K0 * (n+p)
-
-    cst <- log(n)
+    K <- ncol(U)
 
     ### compute criterion
-    res1 <- poisLoglike(X, lambda)
-    res2 <- res1 - (nu_K/2) * (cst) # + 2*log(2) - log(2*pi))
+    res1 <- poisLoglike(X, lambda) - K * (n+p) * log(n*p)
 
-    return(c(res1, res2))
+    return(res1)
 }
 
 
@@ -97,115 +82,3 @@ gammaLoglike = function(X, shape, rate) {
 
     return(sum( (shape.ij - 1)*log(X.ij)  + shape.ij*log(rate.ij) - rate.ij * X.ij - lgamma(shape.ij) ))
 }
-
-jeffreys <- function(shape,rate) {
-    return(sum(0.5*log(sqrt(trigamma(shape)*shape -1)) - log(rate)))
-}
-
-
-
-icl2 <- function(model, X) {
-
-    n <- nrow(X)
-    p <- ncol(X)
-
-    myOrder <- model$order$orderDeviance
-
-    U <- model$U[,myOrder]
-    V <- model$V[,myOrder]
-
-    K <- ncol(U)
-
-    # alpha1 <- model$params$alpha1
-    # alpha2 <- model$params$alpha2
-    #
-    # beta1 <- model$params$beta1
-    # beta2 <- model$params$beta2
-
-    alpha1 <- model$params$phi1
-    alpha2 <- model$params$phi2
-
-    beta1 <- model$params$theta1
-    beta2 <- model$params$theta2
-
-    criterion <- sapply(1:K, function(k) {
-
-        nu_K <- k * (n+p)
-
-        cst <- log(n*p)
-        # cst <- log(n)
-
-        Utmp <- as.matrix(U[,1:k])
-        Vtmp <- as.matrix(V[,1:k])
-        alpha1tmp <- as.matrix(alpha1[,1:k])
-        alpha2tmp <- as.matrix(alpha2[,1:k])
-        beta1tmp <- as.matrix(beta1[,1:k])
-        beta2tmp <- as.matrix(beta2[,1:k])
-
-        lambda <- Utmp %*% t(Vtmp)
-
-        ### compute criterion
-        res1 <- poisLoglike(X, lambda) + gammaLoglike(Utmp, alpha1tmp, alpha2tmp) + gammaLoglike(Vtmp, beta1tmp, beta2tmp) - (nu_K/2) * (cst + 2*log(2) + log(2*pi))
-        res2 <- poisLoglike(X, lambda) + gammaLoglike(Utmp, alpha1tmp, alpha2tmp) + gammaLoglike(Vtmp, beta1tmp, beta2tmp)
-
-        return(c(res1,res2))
-    })
-
-    return(t(criterion))
-}
-
-
-bic2 <- function(model, X) {
-
-    n <- nrow(X)
-    p <- ncol(X)
-
-    myOrder <- model$order$orderDeviance
-
-    U <- model$U[,myOrder]
-    V <- model$V[,myOrder]
-
-    K <- ncol(U)
-
-    # alpha1 <- model$params$alpha1
-    # alpha2 <- model$params$alpha2
-    #
-    # beta1 <- model$params$beta1
-    # beta2 <- model$params$beta2
-
-    alpha1 <- model$params$phi1
-    alpha2 <- model$params$phi2
-
-    beta1 <- model$params$theta1
-    beta2 <- model$params$theta2
-
-    criterion <- sapply(1:K, function(k) {
-
-        nu_K <- k * (n+p)
-
-        cst <- log(n*p)
-        # cst <- log(n)
-
-        Utmp <- as.matrix(U[,1:k])
-        Vtmp <- as.matrix(V[,1:k])
-        alpha1tmp <- as.matrix(alpha1[,1:k])
-        alpha2tmp <- as.matrix(alpha2[,1:k])
-        beta1tmp <- as.matrix(beta1[,1:k])
-        beta2tmp <- as.matrix(beta2[,1:k])
-
-        lambda <- Utmp %*% t(Vtmp)
-
-        ### compute criterion
-        res1 <- poisLoglike(X, lambda) - (nu_K/2) * (cst + 2*log(2) + log(2*pi))
-
-        return(res1)
-    })
-
-    return(criterion)
-}
-
-
-
-
-
-
--- scripts/tests/test_model_selection.R
+++ scripts/tests/test_model_selection.R
@@ -1,10 +1,8 @@
 
 rm(list=ls())
 
-RDIR <- system("git rev-parse --show-toplevel", intern=TRUE)
-source(paste0(RDIR, "/set_working_dir.R"))
+source("/home/durif/source_code/countMatrixFactor/set_working_dir.R")
 source("sources/projectSources.R")
-source("draft/modelSelection.R")
 
 # library(fields)
 
@@ -13,9 +11,9 @@ source("draft/modelSelection.R")
 ######################
 
 ## generating the data
-n = 400
+n = 80
 p = 50
-K = 15
+K = 10
 
 
 
@@ -43,9 +41,9 @@ cbind(apply(data1$X,2, function(x) sum(x!=0)), prob1)
 # matrixHeatmap(data1$X, xlab="j = 1...p", ylab="i = 1...n")
 # matrixHeatmap(data1$U, xlab="k = 1...K", ylab="i = 1...n")
 
-####### TESTING ALGO 1
-Kmax=25
-tmp_model <- lapply(1:Kmax, function(k) {
+####### TESTING ALGO
+
+criterion <- sapply(1:20, function(k) {
     ncomp=k
     alpha01 = matrix(1, nrow=n, ncol=ncomp)
     alpha02 = matrix(1, nrow=n, ncol=ncomp)
@@ -60,87 +58,14 @@ tmp_model <- lapply(1:Kmax, function(k) {
                         alpha01, alpha02, beta01, beta02,
                         iterMax=200, epsilon=1e-5, verbose=FALSE,
                         ZI=FALSE, algo = "EM")
-    return(res1)
-})
 
-source("draft/modelSelection.R")
-criterion <- sapply(1:Kmax, function(k) {
-    ncomp=k
-    res1=tmp_model[[k]]
     return(c(tail(res1$deviance,1), tail(res1$logLikelihood$elbo,1), icl(res1, data1$X), bic(res1, data1$X)))
 })
 
-layout(matrix(1:6, nrow=2))
-
-plot(criterion[1,], type="b", main="deviance")
-abline(v=K, col="red")
-plot(criterion[2,], type="b", main="elbo")
-abline(v=K, col="red")
-plot(criterion[3,], type="b", main="joint loglike")
-abline(v=K, col="red")
-plot(criterion[4,], type="b", main="icl1",
-     ylim=c(min(criterion[4:5,]),max(criterion[4:5,])))
-points(criterion[5,], type="b", main="icl2", col="blue", pch=2)
-abline(v=K, col="red")
-plot(criterion[6,], type="b", main="cond loglike")
-abline(v=K, col="red")
-plot(criterion[7,], type="b", main="bic")
-abline(v=K, col="red")
-
-####### TESTING ALGO 1 bis
-source("draft/modelSelection.R")
-criterion <- sapply(1:20, function(k) {
-    ncomp=k
-    alpha01 = matrix(1, nrow=n, ncol=ncomp)
-    alpha02 = matrix(1, nrow=n, ncol=ncomp)
-    beta01 = matrix(1, nrow=p, ncol=ncomp)
-    beta02 = matrix(1, nrow=p, ncol=ncomp)
-    phi01 = matrix(1, nrow=n, ncol=ncomp)
-    phi02 = matrix(1, nrow=n, ncol=ncomp)
-    theta01 = matrix(1, nrow=p, ncol=ncomp)
-    theta02 = matrix(1, nrow=p, ncol=ncomp)
-    res_tmp <- sapply(1:10, function(i) {
-        res1 = matrixFactor(data1$X, ncomp,
-                            phi01, phi02, theta01, theta02,
-                            alpha01, alpha02, beta01, beta02,
-                            iterMax=200, epsilon=1e-5, verbose=FALSE,
-                            ZI=FALSE, algo = "EM")
-
-        return(c(tail(res1$deviance,1), tail(res1$logLikelihood$elbo,1), icl(res1, data1$X), bic(res1, data1$X)))
-    })
-    return(apply(res_tmp,1,mean))
-})
-
-layout(matrix(1:6, nrow=2))
+layout(matrix(1:5, nrow=1))
 
 plot(criterion[1,], type="b", main="deviance")
 plot(criterion[2,], type="b", main="elbo")
 plot(criterion[3,], type="b", main="icl")
 plot(criterion[4,], type="b", main="joint loglike")
 plot(criterion[5,], type="b", main="bic")
-plot(criterion[6,], type="b", main="cond loglike")
-
-####### TESTING ALGO 2
-ncomp=20
-alpha01 = matrix(1, nrow=n, ncol=ncomp)
-alpha02 = matrix(5, nrow=n, ncol=ncomp)
-beta01 = matrix(1, nrow=p, ncol=ncomp)
-beta02 = matrix(2, nrow=p, ncol=ncomp)
-phi01 = matrix(1, nrow=n, ncol=ncomp)
-phi02 = matrix(2, nrow=n, ncol=ncomp)
-theta01 = matrix(1, nrow=p, ncol=ncomp)
-theta02 = matrix(2, nrow=p, ncol=ncomp)
-res1 = matrixFactor(data1$X, ncomp,
-                    phi01, phi02, theta01, theta02,
-                    alpha01, alpha02, beta01, beta02,
-                    iterMax=200, epsilon=1e-5, verbose=FALSE,
-                    ZI=FALSE, algo = "variational")
-
-icl_res <- icl2(res1, data1$X)
-bic_res <- bic2(res1, data1$X)
-
-layout(matrix(1:3, nrow=1))
-
-plot(1:ncomp, icl_res[,1], type="b", main="icl")
-plot(1:ncomp, icl_res[,2], type="b", main="joint loglike")
-plot(1:ncomp, bic_res, type="b", main="bic")
