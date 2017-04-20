e233bc480143c09688b345d69c3b5036193f44df
Ghislain Durif <ghislain.durif@inria.fr>
2017-04-20 15:51:09 +0200

    running some tests on multi-threading in the sparse case

--- scripts/tests/test_gamPoisFactorEM_sparse.R
+++ scripts/tests/test_gamPoisFactorEM_sparse.R
@@ -1,7 +1,8 @@
 
 rm(list=ls())
 
-source("/home/durif/source_code/countMatrixFactor/set_working_dir.R")
+RDIR <- system("git rev-parse --show-toplevel", intern=TRUE)
+source(paste0(RDIR, "/set_working_dir.R"))
 source("sources/projectSources.R")
 
 # library(fields)
@@ -25,7 +26,7 @@ compos <- function(x, n, fun) {
 
 ## generating the data
 n = 100
-p = 100
+p = 1000
 K = 10
 
 nblockU = 2
@@ -63,15 +64,15 @@ data1 <- dataGeneration(n=n, p=p, K=K, alpha1=alpha1, alpha2=alpha2, beta1=beta1
 str(data1)
 
 ## heatmap
-matrixHeatmap(alpha1, xlab="k = 1...K", ylab="i = 1...n")
-matrixHeatmap(beta1, xlab="k = 1...K", ylab="j = 1...p")
-matrixHeatmap(data1$X, xlab="j = 1...p", ylab="i = 1...n")
-matrixHeatmap(data1$U, xlab="k = 1...K", ylab="i = 1...n")
-matrixHeatmap(data1$V, xlab="k = 1...K", ylab="j = 1...p")
+# matrixHeatmap(alpha1, xlab="k = 1...K", ylab="i = 1...n")
+# matrixHeatmap(beta1, xlab="k = 1...K", ylab="j = 1...p")
+# matrixHeatmap(data1$X, xlab="j = 1...p", ylab="i = 1...n")
+# matrixHeatmap(data1$U, xlab="k = 1...K", ylab="i = 1...n")
+# matrixHeatmap(data1$V, xlab="k = 1...K", ylab="j = 1...p")
 
 ####### TESTING ALGO
 
-ncomp=15
+ncomp=10
 
 alpha01 = matrix(1, nrow=n, ncol=ncomp)
 alpha02 = matrix(1, nrow=n, ncol=ncomp)
@@ -86,42 +87,45 @@ theta01 = matrix(1, nrow=p, ncol=ncomp)
 theta02 = matrix(1, nrow=p, ncol=ncomp)
 
 
-res1 = matrixFactor(data1$X, ncomp, phi01, phi02, theta01, theta02, alpha01, alpha02, beta01, beta02,
-                    iterMax=500, iterMin=100, epsilon=1e-4, algo="EM", verbose=FALSE, sparse=TRUE)
+time1 = system.time(res1 <- matrixFactor(data1$X, ncomp, phi01, phi02, theta01, theta02, alpha01, alpha02, beta01, beta02,
+                                         iterMax=500, iterMin=100, epsilon=1e-4, algo="EM", verbose=FALSE, sparse=TRUE, ncores=1))
 
-res2 = matrixFactor(data1$X, ncomp, phi01, phi02, theta01, theta02, alpha01, alpha02, beta01, beta02,
-                    iterMax=500, iterMin=100, epsilon=1e-4, algo="EM", verbose=FALSE, sparse=FALSE)
+time2 = system.time(res2 <- matrixFactor(data1$X, ncomp, phi01, phi02, theta01, theta02, alpha01, alpha02, beta01, beta02,
+                                         iterMax=500, iterMin=100, epsilon=1e-4, algo="EM", verbose=FALSE, sparse=TRUE, ncores=16))
 
-str(res1)
+print(time1)
+print(time2)
 
-print(res1$criteria_k$kDeviance)
-myOrder = res1$order$orderDeviance
-print(myOrder)
-
-# elbo
-plot(res1$logLikelihood$elbo[-(1:10)], xlab="iteration", ylab="elbo", col="blue", type="l")
-plot(res1$normGap[-(1:10)], xlab="iteration", ylab="norm. gap", col="blue", type="l")
-
-plot(res1$criteria_k$kDeviance, type="l")
-
-plot(res2$logLikelihood$elbo[-(1:10)], xlab="iteration", ylab="elbo", col="blue", type="l")
-plot(res2$normGap[-(1:10)], xlab="iteration", ylab="norm. gap", col="blue", type="l")
-
-plot(res2$criteria_k$kDeviance, type="l")
-
-# graph
-U1 = res1$U[, res1$order$orderDeviance]
-U2 = res2$U[, res2$order$orderDeviance]
-plot(U1, col=blockAlpha1$idRows[data1$orderInd])
-plot(U2, col=blockAlpha1$idRows[data1$orderInd])
-
-matrixHeatmap(U1)
-matrixHeatmap(U2)
-
-# selection
-print(res2$params$theta1)
-
-matrixHeatmap(res1$sparseParams$sparseIndic[,myOrder])
-matrixHeatmap(data1$beta1[data1$orderVar,])
-matrixHeatmap((res1$V * res1$sparseParams$sparseIndic)[,myOrder])
-matrixHeatmap(res2$V[,myOrder])
+# str(res1)
+#
+# print(res1$criteria_k$kDeviance)
+# myOrder = res1$order$orderDeviance
+# print(myOrder)
+#
+# # elbo
+# plot(res1$logLikelihood$elbo[-(1:10)], xlab="iteration", ylab="elbo", col="blue", type="l")
+# plot(res1$normGap[-(1:10)], xlab="iteration", ylab="norm. gap", col="blue", type="l")
+#
+# plot(res1$criteria_k$kDeviance, type="l")
+#
+# plot(res2$logLikelihood$elbo[-(1:10)], xlab="iteration", ylab="elbo", col="blue", type="l")
+# plot(res2$normGap[-(1:10)], xlab="iteration", ylab="norm. gap", col="blue", type="l")
+#
+# plot(res2$criteria_k$kDeviance, type="l")
+#
+# # graph
+# U1 = res1$U[, res1$order$orderDeviance]
+# U2 = res2$U[, res2$order$orderDeviance]
+# plot(U1, col=blockAlpha1$idRows[data1$orderInd])
+# plot(U2, col=blockAlpha1$idRows[data1$orderInd])
+#
+# matrixHeatmap(U1)
+# matrixHeatmap(U2)
+#
+# # selection
+# print(res2$params$theta1)
+#
+# matrixHeatmap(res1$sparseParams$sparseIndic[,myOrder])
+# matrixHeatmap(data1$beta1[data1$orderVar,])
+# matrixHeatmap((res1$V * res1$sparseParams$sparseIndic)[,myOrder])
+# matrixHeatmap(res2$V[,myOrder])
