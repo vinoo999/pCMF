7bd75ff126a17e384ecb6473a302cb9edc8d0b7e
Ghislain Durif <ghislain.durif@inria.fr>
2017-06-26 17:23:12 +0200

    get working dir with git and version

--- admin/check_src.sh
+++ admin/check_src.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-R CMD check pCMF_0.5.tar.gz
+#-------------------------------#
+# SET PATH TO DIRECTORY
+WORKDIR=$(git rev-parse --show-toplevel)
+#-------------------------------#
+
+## current version
+VERSION=$(cat $WORKDIR/Version)
+
+## check
+R CMD check $WORKDIR/sources/pCMF_${VERSION}.tar.gz
--- admin/local_install.sh
+++ admin/local_install.sh
@@ -2,7 +2,7 @@
 
 #-------------------------------#
 # SET PATH TO DIRECTORY
-WORKDIR="$HOME/source_code/pCMF"
+WORKDIR=$(git rev-parse --show-toplevel)
 #-------------------------------#
 
 ## install directory
@@ -11,12 +11,12 @@ INSTALLDIR="$WORKDIR/installDir"
 if [ ! -d $INSTALLDIR ]; then mkdir -p $INSTALLDIR; fi
 
 ## source
-VERSION=`cat $WORKDIR/Version`
+VERSION=$(cat $WORKDIR/Version)
 SRCDIR="$WORKDIR/sources"
 SRC="$SRCDIR/pCMF_${VERSION}.tar.gz"
 
 ## remove former version
-rm -r $INSTALLDIR/pCMF
+rm -rf $INSTALLDIR/pCMF
 
 ## INSTALLATION
 R CMD INSTALL --library=$INSTALLDIR $SRC
